4de61ac9bfadeea95322b53a85e30c9cf9ac9ead
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-07-14 19:46:42 -0400

    added a (fake) sidebar

--- app/sidebar.js
+++ app/sidebar.js
@@ -8,5 +8,11 @@ module.exports = function () {
   return html`
   <body>
     <div>Hello world!</div>
+    <div class="sidebar">
+        <a href="#">About</a>
+        <a href="#">Test</a>
+        <a href="#">Choo</a>
+        <a href="#">Electron</a>
+    </div>
   </body>`;
 };
